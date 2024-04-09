fbe215a184462fd1d2033aac314eb4ce7722ad99
scoavoux <samuel.coavoux@ensae.fr>
2024-04-09 09:40:01 +0000

    workflow with quarto

--- _targets.R
+++ _targets.R
@@ -1,5 +1,6 @@
 # Example _targets.R file:
 library(targets)
+library(tarchetypes)
 
 tar_option_set(
   packages = c("paws", "tidyverse", "arrow"),
@@ -24,22 +25,25 @@ list(
   # 
   # tar_target(endo_leg, compute_endo_leg()),
   # tar_target(exo_press, compute_exo_press()),
-  # tar_target(exo_radio, compute_exo_radio()),
+  tar_target(exo_radio, compute_exo_radio()),
   # tar_target(exo_sc, read_csv("")),
   # 
   # tar_target(exo_pca, compute_exo_pca()),
   
-  tar_target(artists, join_artist(artists_list, endo_leg, exo_press, exo_radio, exo_sc)),
+  tar_target(artists, join_artist(exo_radio)), #endo_leg, exo_press, exo_sc
   tar_target(artists_filtered, filter_artists(artists)),
-  
-  # Prepare user data ------
-  
+
   # Analysis Omni 1 ------
   tar_target(gg_endoleg_bygenre, plot_endoleg_bygenre(artists_filtered)),
   tar_target(gg_exoleg_bygenre, plot_exoleg_bygenre(artists_filtered)),
   tar_target(gg_endoexo_bygenre_scatterplot, plot_endoexo_bygenre_scatterplot(artists_filtered)),
   tar_target(gg_endoexo_corrplot, plot_endoexo_corrplot(artists_filtered)),
-  tar_target(tb_leg_variance, table_leg_variance(artists_filtered))
+  tar_target(tb_leg_variance, table_leg_variance(artists_filtered)),
+  
+  # Report Omni1
+  tar_quarto(mauvais_genre_report, "mauvais_genre.qmd")
+  
+  # Prepare user data ------  
   
   # Analysis Omni 2 ------
 )
