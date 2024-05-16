67392cbc0f1dcf101b41c93d14ed1ece52b560eb
scoavoux <samuel.coavoux@gmail.com>
2024-05-16 14:51:09 +0000

    init LCA/LPA

--- R/make_user_data.R
+++ R/make_user_data.R
@@ -25,19 +25,66 @@ compute_omnivorourness_from_survey <- function(){
   return(omni)
 }
 
+compute_latent_classes_from_survey <- function(nclass){
+  require(tidyverse)
+  require(poLCA)
+  require(conflicted)
+  conflict_prefer("select", "dplyr")
+  conflict_prefer("filter", "dplyr")
+  s3 <- initialize_s3()
+  f <- s3$get_object(Bucket = "scoavoux", Key = "records_w3/survey/RECORDS_Wave3_apr_june_23_responses_corrected.csv")
+  survey <- f$Body %>% rawToChar() %>% data.table::fread() %>% tibble()
+  genre_matrix <- survey %>%
+    select(hashed_id, matches("B_genres_\\d+")) %>%
+    pivot_longer(-hashed_id) %>%
+    filter(value != "") %>%
+    mutate(name = 2) %>% 
+    pivot_wider(names_from = value, values_from = name, values_fill = 1)
+  genres <- names(genre_matrix)[-1]
+  paste0("`", genres, "`", collapse=",")[1]
+  form <- cbind(`Rap français`,`RnB`,`Hip Hop / Rap`,`Pop`,`Variété française`,`Musique de Film`,`Musique Latino`,`Jazz`,`Musique Classique`,`Indé / Alternatif`,`Rock`,`Electro`,`Musiques Africaines`,`Reggae`,`Folk & Acoustique`,`Soul / Funk`,`Musique Brésilienne`,`Musiques Asiatiques`,`Country`,`Métal`,`Musiques Arabes`,`Dance & EDM`,`K-pop`,`Blues`)~1
+  mod <- vector("list", length(nclass))
+  names(mod) <- paste0("k", nclass)
+  for(i in nclass){
+    mod[[paste0("k", i)]] <- poLCA(form, genre_matrix, nclass = i)
+  }
+  return(mod)
+}
+
+compute_latent_classes_from_streams <- function(user_artist_peryear, genres, nclass){
+  require(mclust)
+  require(tidyverse)
+  require(tidytable)
+
+  user_artist_peryear <- user_artist_peryear %>% 
+    group_by(hashed_id, artist_id) %>% 
+    # we use number of play
+    summarize(n_play = sum(n_play)) %>% 
+    ungroup()
+  user_genre_matrix <- user_artist_peryear %>% 
+    left_join(genres) %>% 
+    filter(!is.na(genre)) %>% 
+    group_by(hashed_id, genre) %>% 
+    summarize(n_play = sum(n_play)) %>% 
+    pivot_wider(names_from = genre, values_from = n_play, values_fill = 0)
+  
+  mod <- vector("list", length = length(nclass))
+  names(mod) <- paste0("k", nclass)
+  for(i in nclass){
+    mod[[paste0("k", i)]] <- Mclust(select(user_genre_matrix, -hashed_id), G = i)
+  }
+  return(mod)
+}
+
 
 ## Peterson: sum of genres declared
 ### in survey: sum over genres consumed
 ### in survey: sum over genres liked
 
-## diversity over genres actually listened
-### HHI over stream by genre
 
 ## diversity over mean legitimacy of genres
 ### let us attribute to each artist the average legitimacy of their genre
 ### weighted mean and sd of this,
 ### as highbrow dim (mean) and omnivorous dim (sd)
 
-## diversity over individual legitimacy
-### weighted mean and sd of each artist's average legitimacy
-### as highbrow dim (mean) and omnivorous dim (sd)
+
