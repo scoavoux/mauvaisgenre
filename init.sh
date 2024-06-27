3d0ee046b1185149ae26ef27c89bc0b0e3fdaa07
scoavoux <samuel.coavoux@gmail.com>
2024-06-27 17:33:22 +0000

    remake init c

--- init.sh
+++ init.sh
@@ -5,9 +5,12 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1
 
-# Access to files created by this script
-chown -R ${USERNAME}:${GROUPNAME} ${HOME}
-
 # Go into
 cd ~/work/mauvaisgenre
 R -f init.R
+
+mkdir output
+mkdir output/omni1
+mkdir output/omni2
+# Access to files created by this script
+chown -R ${USERNAME}:${GROUPNAME} ${HOME}
