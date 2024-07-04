609cf204ae65676fba046a10b0438bec5c877009
scoavoux <samuel.coavoux@gmail.com>
2024-07-04 13:26:18 +0000

    table to check how widely played the selected artists are

---
+++ R/make_supplementary_analyses.R
@@ -0,0 +1,12 @@
+make_tbl_coverage <- function(artists_pop, artists_filtered){
+  require(tidyverse)
+  require(tidytable)
+  
+  tb <- artists_pop %>% 
+    left_join(select(artists_filtered, artist_id, genre) %>% mutate(included = TRUE)) %>% 
+    mutate(included = ifelse(is.na(included), FALSE, included)) %>% 
+    summarize(control_f_l_play = sum(control_f_l_play),
+              control_f_n_play = sum(control_f_n_play, na.rm = TRUE), 
+              .by = included)
+  return(tb)
+}
