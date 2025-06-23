63c94f04401971f45adc26535a1e3df2f24eecf1
scoavoux <samuel.coavoux@gmail.com>
2025-06-23 14:55:59 +0000

    small fix

--- R/export_data.R
+++ R/export_data.R
@@ -1,6 +1,6 @@
 export_to_csv <- function(object){
   object_char <- deparse(substitute(object))
-  file <- str_glue("{object_char}.csv")
+  file <- str_glue("data/temp/{object_char}.csv")
   write_csv(object, file)
   return(file)
 }
